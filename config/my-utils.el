df72dd3f077925f0f4936cd8fa87e47bb2371ce9
Piotr Klibert <piotr.klibert@10clouds.com>
2013-10-02 20:23:34 +0200

    Moved grep-todos to utils

--- config/my-utils.el
+++ config/my-utils.el
@@ -1,4 +1,6 @@
 (require 'cl)
+(require 's)
+(require 'dash)
 
 (defun buffer-line (&optional lineno)
   (when lineno
@@ -12,6 +14,8 @@
   (buffer-substring-no-properties (line-beginning-position)
                                   (line-end-position)))
 
+
+
 (defun my-get-region-or-line-bounds ()
   (let ((active? (use-region-p)))
     (list active?
@@ -19,5 +23,56 @@
               (line-beginning-position))
           (or (and active? (region-end))
               (line-end-position)))))
+
+
+(defun magic-dir-p (dir)
+  (member dir (list "." "..")))
+
+(defun walk-d ()
+  (let ((z (directory-files  "~/.emacs.d/"))
+        (s '())
+        (f '()))
+    (while (or s z)
+      (while (magic-dir-p (car z)) (pop z))
+      (when (not z)
+        (setq z (pop s)))
+      (push (car z) f)
+      (if (not (file-directory-p (car z)))
+          (pop z)
+        (when (cdr z)
+          (push (cdr z) s))
+        (setq z (directory-files (car z)))))
+    f))
+
+;; (length (walk-d))
+
+
+
+(defun grep-todos ()
+  "Grep files in config/ directory for TODOs and insert them
+after point."
+  (interactive)
+  (let ((files (--filter (string-match "^[a-z].*.el$" it)
+                         (directory-files "~/.emacs.d/config/")))
+        (todos '()))
+    (loop for file in files
+          do (with-temp-buffer
+               (insert-file-contents file)
+               (goto-char (point-min))
+               (while (condition-case nil
+                          (search-forward "TODO")
+                        (error nil))
+                 (push (list file
+                             (line-number-at-pos)
+                             (buffer-line-np))
+                       todos))))
+    (while (car todos)
+      (let ((todo (pop todos)))
+        (insert (apply 'format "%40s::%6s %s\n" todo) )
+        (open-line 1)))))
+;; (grep-todos)
+
+
+;; find . -not -iname "*.elc" -not -ipath "*.git*" -not -path "*semanticdb*" -type f -exec ls -l \{\} \; | column -t | awk '{print $5}' | sum
 
 (provide 'my-utils)
