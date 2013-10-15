bba69418809e82f94f26a6f1a16dfce6561f9a64
Piotr Klibert <piotr.klibert@10clouds.com>
2013-10-15 20:53:50 +0200

    remember mode templates

--- config/my-org-config.el
+++ config/my-org-config.el
@@ -10,10 +10,51 @@
 (setq org-mobile-directory "~/orgmobile/")
 (setq org-mobile-inbox-for-pull "~/todo/mobile-pull.org")
 
+(setq org-default-notes-file "~/todo/notes")
+
 (require 'org)
 (org-remember-insinuate)
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+
+
+(setq org-remember-templates '(("Todo" ?t "* TODO %? %^g\nAdded: %U\n%i"
+                                "~/todo/todo.org" "TASKS")
+                               ))
+
+;; ("Journal" ?j "\n* %^{topic} %T \n%i%?\n" "L:journal.org")
+;; ("Book" ?b "\n* %^{Book Title} %t :READING: \n%[l:/booktemp.txt]\n"
+;;  "L:journal.org")
+;; ("Private" ?p "\n* %^{topic} %T \n%i%?\n" "F:/gtd/privnotes.org")
+;; ("Contact" ?c "\n* %^{Name} :CONTACT:\n%[l:/contemp.txt]\n"
+;;  "F:/gtd/privnotes.org")
+
+;; (setq org-remember-templates
+;;    '(("Todo" ?t "* TODO %^{Brief Description} %^g\n%?\nAdded: %U" "~/GTD/newgtd.org" "Tasks")
+;;      ("Journal"   ?j "** %^{Head Line} %U %^g\n%i%?"  "~/GTD/journal.org")
+;;      ("Clipboard" ?c "** %^{Head Line} %U %^g\n%c\n%?"  "~/GTD/journal.org")
+;;      ("Receipt"   ?r "** %^{BriefDesc} %U %^g\n%?"   "~/GTD/finances.org")
+;;      ("Book" ?b "** %^{Book Title} %t :BOOK: \n%[~/.book_template.txt]\n"
+;;         "~/GTD/journal.org")
+;;          ("Film" ?f "** %^{Film Title} %t :FILM: \n%[~/.film_template.txt]\n"
+;;         "~/GTD/journal.org")
+;;      ("Daily Review" ?a "** %t :COACH: \n%[~/.daily_review.txt]\n"
+;;         "~/GTD/journal.org")
+;;      ("Someday"   ?s "** %^{Someday Heading} %U\n%?\n"  "~/GTD/someday.org")
+;;      ("Vocab"   ?v "** %^{Word?}\n%?\n"  "~/GTD/vocab.org")
+;;     )
+;;   )
+
+
+
+
+
 
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c r") 'org-remember)
+
 
 (defun my-org-hook ()
   (define-key org-mode-map (kbd "C-c <up>")     'outline-previous-visible-heading)
