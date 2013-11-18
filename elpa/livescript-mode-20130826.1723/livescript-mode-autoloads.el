b16d6991d357844624fe70e748581552e7a1fa13
Piotr Klibert <piotr.klibert@10clouds.com>
2013-11-19 00:27:14 +0100

    Added mode for LiveScript

---
+++ elpa/livescript-mode-20130826.1723/livescript-mode-autoloads.el
@@ -0,0 +1,28 @@
+;;; livescript-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "livescript-mode" "livescript-mode.el" (21126
+;;;;;;  14400 0 0))
+;;; Generated autoloads from livescript-mode.el
+
+(autoload 'livescript-mode "livescript-mode" "\
+Major mode for editing LiveScript code.
+
+Commands:
+
+\\{livescript-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.ls\\'" . livescript-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; livescript-mode-autoloads.el ends here
---
+++ elpa/livescript-mode-20130826.1723/livescript-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "livescript-mode" "20130826.1723" "Major mode for editing LiveScript files [github]" 'nil)
