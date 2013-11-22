7765e93484919e9116030765aaf88afe28a39e28
Piotr Klibert <piotr.klibert@10clouds.com>
2013-11-22 16:42:10 +0100

    Tagasuris ticket name setter, something

--- config/my-vcs-config.el
+++ config/my-vcs-config.el
@@ -9,11 +9,16 @@
 (define-key mode-specific-map (kbd "C-M-g") 'magit-blame-mode)     ; C-c C-M-g
 
 (defvar my-tagasauris-ticket-name nil)
-;; (setq my-tagasauris-ticket-name "HAC-77")
+;; (setq my-tagasauris-ticket-name "RPA-5")
+
+(defun my-tagasauris-set-ticket (ticket)
+  (interactive "sNew ticket name: ")
+  (setq my-tagasauris-ticket-name ticket))
 
 (defun my-magit-commit-hook ()
   (unless my-tagasauris-ticket-name
     (setq my-tagasauris-ticket-name
           (read-string "Ticket name: ")))
   (goto-char (point-min))
-  (insert my-tagasauris-ticket-name " "))
+  (unless (= 0 (length my-tagasauris-ticket-name)) ; don't insert a space is not needed
+    (insert my-tagasauris-ticket-name " ")))
