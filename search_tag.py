4b90aeb742603d62a7f456b48c99522da5904289
Piotr Klibert <piotr.klibert@10clouds.com>
2013-12-12 00:06:20 +0100

    whoosh search

--- search_tag.py
+++ search_tag.py
@@ -1,14 +1,29 @@
+import re
+from time import time
 from path import path as P
 from whoosh.index import open_dir
 from whoosh.query import Regex
 
 index = open_dir("./idx")
 
-with index.searcher() as s:
-    results = s.search(Regex("content", "^class$"), limit=None)
-    for result in results:
-        if not result["content"].startswith("class"):
-            continue
-        p = P(result["path"])
-        # print "/".join(p.splitall()[-3:]),
-        # print result["line"], result["content"][:-2]
+
+def search(term):
+    with index.searcher() as s:
+        results = s.search(Regex("content", term), limit=None)
+        regexp = re.compile(term)
+
+        for result in results:
+            if not regexp.search(result["content"]):
+                print ",",
+                continue
+            p = P(result["path"])
+            print "/".join(p.splitall()[-3:]),
+            print result["line"], result["content"][:-1]
+
+
+
+if __name__ == "__main__":
+    t = time()
+    import sys
+    search(sys.argv[1])
+    print time() - t
