2d32ae0e20d2471f4a95e969b9e022114fd7bed9
Piotr Klibert <piotr.klibert@10clouds.com>
2013-12-28 16:46:12 +0100

    Removed all and all-ext, because occur does this already

--- elpa/all-ext-20130824.1206/all-ext-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; all-ext-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("all-ext-pkg.el" "all-ext.el") (21058
-;;;;;;  3610 483717 503000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; all-ext-autoloads.el ends here
