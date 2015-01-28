23220eed2a8bcf002fa3c90a4d4afe1f1274e2cc
Piotr Klibert <piotr.klibert@10clouds.com>
2015-01-28 22:24:13 +0100

    popup is in forked-plugins already

--- elpa/popup-0.5/popup-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21183 30310
-;;;;;;  322593 902000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
