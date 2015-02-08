9e10ad38d1d8cb703a5a26626178097239980544
Piotr Klibert <piotr.klibert@10clouds.com>
2015-02-08 13:30:21 +0100

    A little cleanup in ffip paths

--- config/my-projects.el
+++ config/my-projects.el
@@ -5,18 +5,19 @@
 ;; explicitly
 
 
-(ffip-defroots 'prv ("~/todo/" "~/secure/" "~/.emacs.d/config/")
+(ffip-defroots 'my-project ("~/todo/" "~/secure/" "~/.emacs.d/config/")
   (my-projects . ("~/mgmnt/"
+                  "~/projects/klibert_pl/"
                   ;; "~/projects/open-resty/"
                   ;; "~/projects/images/my-base/"
-                  "~/projects/klibert_pl/"
-                  "~/projects/reqplayer/dpath/"
-                  "~/poligon/plnizator/"
-                  "~/poligon/luzem/"
-                  "~/poligon/lscript/"
+                  ;; "~/projects/reqplayer/dpath/"
+                  ;; "~/poligon/plnizator/"
+                  ;; "~/poligon/luzem/"
+                  ;; "~/poligon/lscript/"
                   ))
   (emacs       . ("~/.emacs.d/pkg-langs/elpy/"
-                  "~/.emacs.d/plugins2/" "~/.emacs.d/pkg-langs/"))
+                  "~/.emacs.d/plugins2/"
+                  "~/.emacs.d/pkg-langs/"))
 
   ;; WORK RELATED
   (ion         . ("~/projects/ion/"))
@@ -33,24 +34,29 @@
 
   (10c         . ("~/projects/images/repo/")))
 
-;; There's a bit of a mess in my "misc projects" folder, and some of its
-;; directories have much too many files in them, so I need to prune them before
-;; adding them to `fuzzy-find-roots'.
-(require 'f)
 
-(defun my-absolutize (path dirname)
-  (f-expand (f-join path dirname)))
 
-(lexical-let*
-    ((ignored (--map (f-expand (f-join "~/poligon/" it))
-                     '("books-dedup" "django-debug-toolbar"
-                       "django-rest-framework" "haxe"
-                       "old_web_app_template" "poligon")))
-     (subdirs (f-directories (f-expand "~/poligon/")
-                             (lambda (path)
-                               (not (-contains? ignored path)))))
-     (new-ffip-dirs (append (util-get-alist 'my-projects fuzzy-find-roots)
-                            subdirs)))
-  (util-put-alist 'my-projects new-ffip-dirs fuzzy-find-roots)
-  ;; make FFIP notice the change in in dirs
-  (fuzzy-find-choose-root-set "prv"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NOTE: disabled for now because my "misc projects" folder became TOO MUCH of
+;; a mess...
+;;
+;; ;; There's a bit of a mess in my "misc projects" folder, and some of its
+;; ;; directories have much too many files in them, so I need to prune them before
+;; ;; adding them to `fuzzy-find-roots'.
+;; (require 'f)
+;; (defun my-absolutize (path dirname)
+;;   (f-expand (f-join path dirname)))
+;; (lexical-let*
+;;     ((ignored (--map (f-expand (f-join "~/poligon/" it))
+;;                      '("books-dedup" "django-debug-toolbar"
+;;                        "django-rest-framework" "haxe"
+;;                        "old_web_app_template" "poligon")))
+;;      (subdirs (f-directories (f-expand "~/poligon/")
+;;                              (lambda (path)
+;;                                (not (-contains? ignored path)))))
+;;      (new-ffip-dirs (append (util-get-alist 'my-projects fuzzy-find-roots)
+;;                             subdirs)))
+;;   (util-put-alist 'my-projects new-ffip-dirs fuzzy-find-roots)
+;;   ;; make FFIP notice the change in in dirs
+;;   (fuzzy-find-choose-root-set "my-projects"))
