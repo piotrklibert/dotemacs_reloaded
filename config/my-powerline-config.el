5e578fbff5b6e7c8b20a7071988d0ce98d33de48
Piotr Klibert <piotr.klibert@10clouds.com>
2016-08-23 00:35:25 +0200

    Some cleanup

--- config/my-powerline-config.el
+++ config/my-powerline-config.el
@@ -10,16 +10,6 @@
       p
     (concat (substring p 0 2) ".." (substring p -2))))
 
-(defun my-shorten-path/drop-segments (path max-size)
-  (if (< (length path) max-size)
-      path
-    (let ((p (s-split "/" path))
-          (idx 0))
-      (while (and (> (length (my-join-path p)) max-size)
-                  (> (length p) 2))
-        (setf p (cdr p)))
-      (my-join-path p))))
-
 (defun my-shorten-path/shorten-segments (path max-size)
   (if (< (length path) max-size)
       path
@@ -31,9 +21,25 @@
         (setf idx (1+ idx)))
       (my-join-path p))))
 
-;; (my-shorten-path "Users/piotrklibert/projects/bunsen/api/resources/blog/migrations/20160414113000-file-object.up.sql" 35)
-;; (my-shorten-path-segment "piotrklibert")
-;; (my-shorten-path "Users/piotrklibert/.emacs.d/config/my-powerline-config.el" 35)
+;; (my-shorten-path/shorten-segments
+;;  "Users/piotrklibert/projects/bunsen/api/resources/blog/migrations/20160414113000-file-object.up.sql" 35)
+;; => "Users/pi..rt/pr..ts/bunsen/api/re..es/blog/mi..ns/20..ql"
+
+
+(defun my-shorten-path/drop-segments (path max-size)
+  (if (< (length path) max-size)
+      path
+    (let ((p (s-split "/" path))
+          (idx 0))
+      (while (and (> (length (my-join-path p)) max-size)
+                  (> (length p) 2))
+        (setf p (cdr p)))
+      (my-join-path p))))
+
+;; (my-shorten-path/drop-segments
+;;  "Users/piotrklibert/projects/bunsen/api/resources/blog/migrations/20160414113000-file-object.up.sql" 35)
+;; => "migrations/20160414113000-file-object.up.sql"
+
 
 (defun my-pl-format-dir (buffer-path)
   (->> buffer-path
@@ -51,7 +57,9 @@
         (concat "/"))
     (error "")))
 
+
 (defvar my-powerline-max-path-length 30)
+
 
 (defpowerline powerline-buffer-id
   (let ((buffer-ident (powerline-trim
