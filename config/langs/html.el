7067672994ffd2221bdc46edef811d9fa26c9b63
Piotr Klibert <piotr.klibert@10clouds.com>
2018-09-01 22:00:53 +0200

    Make C-c C-/ work in web-mode also

--- config/langs/html.el
+++ config/langs/html.el
@@ -13,3 +13,16 @@
   (add-hook 'sgml-mode-hook 'my-sgml-mode-hook)
   ;; somehow flymake doesn't want to work
   (delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks))
+
+(require 'web-mode-autoloads)
+
+(defun my-web-close-tag ()
+  (interactive)
+  (web-mode-element-close))
+
+(defvar web-mode-map)
+(defun my-web-mode-hook ()
+  (define-key web-mode-map (kbd "C-c /") 'web-mode-element-close) ; + (indent-for-tab-command)
+  (define-key web-mode-map (kbd "C-c C-/") 'web-mode-element-close)
+  )
+(add-hook 'web-mode-hook 'my-web-mode-hook)
